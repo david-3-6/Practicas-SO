8ccb98b17942f89c3cfe533d6c5056949ffac12b
david-3-6 <dcuestamartin@gmail.com>
2023-02-09 16:27:26 +0100

    5

--- Practica2/ejemplofor.sh
+++ Practica2/ejemplofor.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/bash
+for i in *
+do
+    if [ -d $i  ]
+    then
+        echo -e "$i \tdirectorio"
+    elif [ -f $i  ]
+    then
+        echo -e "$i \tfichero"
+    else
+        echo -e "$i \tarchivo especial"
+    fi
+done
